55d7ffd0fc8836f3b4e41ac9ae71cfb3bcc457cd
tempfebrihedy <42461207+tempfebrihedy@users.noreply.github.com>
2018-08-17 19:02:14 +0700

    Add files via upload

---
+++ exercise-14.js
@@ -0,0 +1,53 @@
+function sorting(arrNumber) {
+    return result = arrNumber.sort(function(a, b) { return a < b});
+}
+  
+
+
+function getTotal(arrNumber) {
+    var total = 0;
+    if (arrNumber.length === 0) {
+        return '';
+    }
+    else {
+        for (var i=0; i<arrNumber.length; i++) {
+            // console.log(arrNumber[i], arrNumber[0]);
+            if (arrNumber[i] === arrNumber[0]) {
+                total += 1;
+            }
+        }
+        return "angka paling besar adalah "+ arrNumber[0] +" dan jumlah kemunculan sebanyak "+ total +" kali";
+    }
+}
+
+
+  
+function mostFrequentLargestNumbers(arrNumber) {
+    var listSort = sorting(arrNumber);
+    var countHighest = getTotal(listSort);
+    return countHighest;
+}
+  
+console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+//   //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+  
+console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
+//   //'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+  
+console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+//   //'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+  
+console.log(mostFrequentLargestNumbers([]));
+//   //''
+
+
+
+  /*
+2, 8, 4
+compare index i & i+1
+if i+1>i
+angka[i] = angka [i+1]
+angka[i+1] = angka[i]
+
+
+  */
